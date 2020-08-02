ca2ad035d108de35d064007ca64b4c058070945c
Raúl LARA ARRANZ <raullaraarranz@Rauls-MacBook-Pro.local>
2020-08-02 13:13:38 +0200

    initial commit

---
+++ src/test/java/com/reactor/webfluxeureka/WebfluxEurekaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.reactor.webfluxeureka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebfluxEurekaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
